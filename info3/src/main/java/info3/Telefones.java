09dc03f2184835b09aee4b8b56ed0c63aac07f9f
Emanuel Vitor Felix de Oliveira <81042722+N3M3S8S@users.noreply.github.com>
2025-02-06 17:51:59 -0300

    Aula 06-02
    Adição da tabela telefones

---
+++ info3/src/main/java/info3/Telefones.java
@@ -0,0 +1,72 @@
+package info3;
+
+//Esta classe abstrai a tabela telefones do banco de dados
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class Telefones {
+    private String idtelefones;
+    private String idusuarios;
+    private Usuario usuario;
+    private String telefone;
+
+    //metodo que é executado ao instanciar a classe (construtor)
+    public Telefones() {
+        
+    }
+    
+    public void cadastrar () throws SQLException {
+        Connection conexao = Conexao.getConexao();
+        String sql = "INSERT INTO telefones (idusuario, telefones) VALUES (?, ?)";
+        //Filtrando ataques ao banco de dados
+        
+        PreparedStatement comando = conexao.prepareStatement(sql, PreparedStatement.RETURN_GENERATED_KEYS);
+        comando.setString(1, this.idusuarios);
+        comando.setString(2, this.telefone);
+        comando.executeUpdate();
+        
+        //Recuperando chave utilizada
+        ResultSet resultado = comando.getGeneratedKeys();
+        
+        if (resultado.next()) {
+            
+        }
+    }
+    
+    public String getIdtelefones() {
+        return idtelefones;
+    }
+
+    public void setIdtelefones(String idtelefones) {
+        this.idtelefones = idtelefones;
+    }
+
+    public String getIdusuarios() {
+        return idusuarios;
+    }
+
+    public void setIdusuarios(String idusuarios) {
+        this.idusuarios = idusuarios;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+    
+    
+}
---
+++ table-phones.sql
@@ -0,0 +1,38 @@
+SHOW SCHEMAS;
+
+CREATE SCHEMA revisao2;
+
+USE revisao2;
+
+SHOW TABLES;
+
+CREATE TABLE usuario (
+	id int not null primary key auto_increment,
+    nome varchar(255),
+    senha varchar(255)
+);
+
+CREATE TABLE telefones (
+	idtelefone int not null primary key auto_increment,
+	idusuario int not null,
+    telefone varchar(255),
+    foreign key (idusuario) REFERENCES usuario(idusuario)
+);
+
+describe usuario;
+
+INSERT INTO usuario (id, nome, senha) values (1,"Emanuel","Teste");
+
+select * FROM usuario;
+
+-- Aula
+
+use info3;
+
+drop table telefones;
+
+show tables;
+
+(Select nome AS nome2 FROM usuario2) union (select id as codigo FROM usuario);
+
+describe usuario2
